989055d2a988eeedf18dc283ecfd6382c42be152
Pramesh05 <111199674+Pramesh05@users.noreply.github.com>
2022-10-20 21:02:23 +0530

    ☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒
    ☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒

--- settings.js
+++ settings.js
@@ -39,99 +39,24 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.vcardowner = ['94704472592'] //⚙️⚙️
-global.ownername = "𝙥𝙧𝙖𝙢𝙚𝙨𝙝 𝙣𝙞𝙡𝙖𝙠𝙨𝙝𝙖𝙣" //එම අයිතිකරුගේ නම මෙතන්ට දාන්න
-//මේව වෙනස් කරන්න පුලුවන්😊
+global.ownername = "𝙥𝙧𝙖𝙢𝙚𝙨𝙝 𝙣𝙞𝙡𝙖𝙠𝙨𝙝𝙖𝙣" //bot ගෙ අයිති කරුගෙ නම
 global.owner = ['94704472592']
-global.ownertag = '94704472592' //මේ නම්බර් එකට ඔයාගෙ නම්බර එක දාගන්න කව්රුහරි මේකෙ masege  එකක් tag කරාම sticker එනව
-global.botscript = 'https://chat.whatsapp.com/E3enguWGKzLJT0PES2AhrY' //group link
+global.ownertag = '94704472592' //මේ නම්බර් එකට ඔයාගෙ number එක දාගන්න ඔයාව කව්රු හරි tag කලාම sticker එකක් වැටෙනව
+global.botscript = 'https://sites.google.com/view/pramesh-lion-bot/%E0%B6%B1%E0%B7%80%E0%B7%83?read_current=1'
 
 global.socialm = "GitHub: lion" //ur github or insta name
 global.location = "srilanka,colombo" //ur location
-// මෙතනින් යට මොනවත් වෙනස් කරන්න එපා 🚫⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
-//වැඩ කරන්නෙ නැතුව යනව
-//☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒//
 
-global.lion = "🪀" //☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒
-global.menumoji = "🪀" //☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒ mekawath hariyanna
-global.linkz = "https://youtube.com/channel/UCOlt2_XK6tS2KJn6fTdpPew" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtube.com/channel/UCOlt2_XK6tS2KJn6fTdpPew" //ur website to be displayed
-global.reactmoji = "🦁" //ur menu react emoji code lock🔒
-global.themeemoji = "🦁" //ur theme emoji code
-global.packname = "☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒" //ur sticker watermark packname code lock 🔒
-global.author = "☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒" //ur sticker watermark author code lock 🔒
-global.botname = '☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒' //code lock 🔒
-global.ytname = "pramesh lion bot tech" // code lock 🔒
+global.reactmoji = "🦁" //ur menu react emoji 
+global.themeemoji = "🦁" //ur theme emoji 
+global.menumoji = "🪀"//මේක දාද්දි Menu command  එක ඉස්සරහින් මේ imoji එක වැටෙනව
+global.lion = "💝💝💝💝💝💝"
+global.react = "🙋" //owner ractemoji🙋 මේක දාද්දි ඔයා දාන හැම මැසෙජ් එකකටම bot react කරනව🎸
+global.packname = "☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒" 
+global.author = "☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒" 
+global.botname = '☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒'
+global.ytname = "pramesh lion bot tech" 
 
-//Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
-//damtabase
-global.premium = ['916909137213'] //ur premium numbers
-global.ntilinkytvid = []
-global.ntilinkytch = []
-global.ntilinkig = []
-global.ntilinkfb = []
-global.ntilinktg = []
-global.ntilinktt = []
-global.ntilinktwt = []
-global.ntilinkall = []
-global.nticall = []
-global.ntwame = []
-global.nttoxic = []
-global.ntnsfw = []
-global.ntvirtex = []
-global.rkyt = []
-global.wlcm = []
-global.gcrevoke = []
-global.autorep = []
-global.ntilink = []
-
-//omther
-global.sessionName = 'session'
-global.antitags = true
-global.prefa = ['','!','.','🦁','⚙️','✅']
-global.sp = '⭔'
-global.mess = {
-    success: 'හරි ✅',
-    admin: 'මෙය කල හැක්කෙ grop  admin ට පමනයි💻',
-    botAdmin: 'මෙය කිරීමට මට admin අවශ්‍ය වේ ⚙️',
-    owner: 'මෙය කල හැක්කෙ මාගෙ අයිති කරුට පමනයි 🌚',
-    group: 'මෙම විදානයා පාවිච්චි කල හැක්කෙ grop වලදී පමනයි 👥👥',
-    private: 'මෙය කිරීම සදහා මාගෙ inbox පැමිනෙන්න',
-    bot: 'This Feature Is Only For Bot',
-    wait: ' මෙය සාදමින් පවතී  🛠️🛠️',
-    linkm: 'Where is the link?',
-    error: 'එහි දෝෂ පවතී 💥',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
-}
-    global.limitawal = {
-    premium: "Infinity",
-    free: 12,
-    monayawal: 1000
-}
-   global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
-}
-global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})
+//🚫මෙතනින් යට වෙනස් කරන්න එපා🚫\\
+const _0x59148d=_0x213e;function _0x3fe6(){const _0x4e1f30=['එහි\x20දෝෂ\x20පවතී\x20💥','nticall','watchFile','thum','premium','ntilinktwt','ntnsfw','Infinity','ntilinkall','The\x20nsfw\x20feature\x20has\x20not\x20been\x20activated,\x20please\x20contact\x20the\x20admin\x20to\x20activate','mess','fluming','621320SoxRvz','./XeonMedia/theme/cheemspic.jpg','redBright','nttoxic','12XdWwQt','thumb','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','prefa','cache','\x20මෙය\x20සාදමින්\x20පවතී\x20\x20🛠️🛠️','ntilinktt','session','2ijFedT','You\x20have\x20been\x20banned\x20by\x20the\x20owner,\x20if\x20you\x20want\x20to\x20be\x20unbanned,\x20chat\x20owner.','resolve','4728110EQRXPs','246449RqCLoO','9VspvqF','unwatchFile','Update\x27','Your\x20Daily\x20Limit\x20Has\x20Expired,\x20The\x20Limit\x20Will\x20Be\x20Reset\x20Every\x2012\x20Hours','gcrevoke','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','The\x20bot\x20was\x20banned\x20in\x20this\x20group,\x20please\x20contact\x20the\x20owner\x20to\x20unban','wlcm','2318090cYXHcv','ntvirtex','antitags','https://youtube.com/channel/UCOlt2_XK6tS2KJn6fTdpPew','ntilinktg','2012584HQWksw','මෙය\x20කිරීමට\x20මට\x20admin\x20අවශ්‍ය\x20වේ\x20⚙️','https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=','websitex','88585BNxGYe','94704472592','linkz','612063WfjRRn','err4r','flaming','log','limitawal','readFileSync','ntilinkytvid','ntilinkig','ntilink','මෙය\x20කල\x20හැක්කෙ\x20grop\x20\x20admin\x20ට\x20පමනයි💻','ntwame','sessionName'];_0x3fe6=function(){return _0x4e1f30;};return _0x3fe6();}function _0x213e(_0x23936c,_0x1f7c05){const _0x3fe69c=_0x3fe6();return _0x213e=function(_0x213e72,_0xae2876){_0x213e72=_0x213e72-0x154;let _0x3fb3c5=_0x3fe69c[_0x213e72];return _0x3fb3c5;},_0x213e(_0x23936c,_0x1f7c05);}(function(_0x26639e,_0x5e9703){const _0x15e565=_0x213e,_0x1b6732=_0x26639e();while(!![]){try{const _0x15eda6=parseInt(_0x15e565(0x184))/0x1+-parseInt(_0x15e565(0x16e))/0x2*(-parseInt(_0x15e565(0x187))/0x3)+parseInt(_0x15e565(0x162))/0x4+parseInt(_0x15e565(0x17b))/0x5+-parseInt(_0x15e565(0x166))/0x6*(-parseInt(_0x15e565(0x172))/0x7)+-parseInt(_0x15e565(0x180))/0x8*(parseInt(_0x15e565(0x173))/0x9)+-parseInt(_0x15e565(0x171))/0xa;if(_0x15eda6===_0x5e9703)break;else _0x1b6732['push'](_0x1b6732['shift']());}catch(_0x15db7a){_0x1b6732['push'](_0x1b6732['shift']());}}}(_0x3fe6,0x3ee30),global[_0x59148d(0x159)]=fs[_0x59148d(0x18c)](_0x59148d(0x163)),global['log0']=fs[_0x59148d(0x18c)](_0x59148d(0x163)),global[_0x59148d(0x188)]=fs[_0x59148d(0x18c)](_0x59148d(0x163)),global[_0x59148d(0x167)]=fs[_0x59148d(0x18c)](_0x59148d(0x163)),global[_0x59148d(0x15a)]=[_0x59148d(0x185)],global[_0x59148d(0x18d)]=[!![]],global['ntilinkytch']=[],global[_0x59148d(0x18e)]=[],global['ntilinkfb']=[],global[_0x59148d(0x17f)]=[],global[_0x59148d(0x16c)]=[],global[_0x59148d(0x15b)]=[],global[_0x59148d(0x15e)]=[],global[_0x59148d(0x157)]=[],global[_0x59148d(0x154)]=[],global[_0x59148d(0x165)]=[],global[_0x59148d(0x15c)]=[],global[_0x59148d(0x17c)]=[],global['rkyt']=[],global[_0x59148d(0x17a)]=[],global[_0x59148d(0x177)]=[],global['autorep']=[],global[_0x59148d(0x18f)]=[],global[_0x59148d(0x186)]='https://youtube.com/channel/UCOlt2_XK6tS2KJn6fTdpPew',global[_0x59148d(0x183)]=_0x59148d(0x17e),global[_0x59148d(0x155)]=_0x59148d(0x16d),global[_0x59148d(0x17d)]=!![],global[_0x59148d(0x169)]=['','!','.','🦁','⚙️','✅'],global['sp']='⭔',global[_0x59148d(0x160)]={'success':'හරි\x20✅','admin':_0x59148d(0x190),'botAdmin':_0x59148d(0x181),'owner':'මෙය\x20කල\x20හැක්කෙ\x20මාගෙ\x20අයිති\x20කරුට\x20පමනයි\x20🌚','group':'මෙම\x20විදානයා\x20පාවිච්චි\x20කල\x20හැක්කෙ\x20grop\x20වලදී\x20පමනයි\x20👥👥','private':'මෙය\x20කිරීම\x20සදහා\x20මාගෙ\x20inbox\x20පැමිනෙන්න','bot':'This\x20Feature\x20Is\x20Only\x20For\x20Bot','wait':_0x59148d(0x16b),'linkm':'Where\x20is\x20the\x20link?','error':_0x59148d(0x156),'endLimit':_0x59148d(0x176),'ban':_0x59148d(0x16f),'nsfw':_0x59148d(0x15f),'banChat':_0x59148d(0x179)},global[_0x59148d(0x18b)]={'premium':_0x59148d(0x15d),'free':0xc,'monayawal':0x3e8},global['rpg']={'darahawal':0x64,'besiawal':0xf,'goldawal':0xa,'emeraldawal':0x5,'umpanawal':0x5,'potionawal':0x1},global[_0x59148d(0x167)]=fs['readFileSync'](_0x59148d(0x163)),global[_0x59148d(0x189)]=_0x59148d(0x178),global[_0x59148d(0x161)]='https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=',global['flarun']=_0x59148d(0x168),global['flasmurf']=_0x59148d(0x182));let file=require[_0x59148d(0x170)](__filename);fs[_0x59148d(0x158)](file,()=>{const _0x2fae2e=_0x59148d;fs[_0x2fae2e(0x174)](file),console[_0x2fae2e(0x18a)](chalk[_0x2fae2e(0x164)](_0x2fae2e(0x175)+__filename+'\x27')),delete require[_0x2fae2e(0x16a)][file],require(file);});
